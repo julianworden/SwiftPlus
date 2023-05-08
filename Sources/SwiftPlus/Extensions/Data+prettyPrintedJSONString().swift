d74acda68baebee2d58c2168fcf77d3e7921e22b
Julian Worden <93101937+julianworden@users.noreply.github.com>
2023-05-08 06:56:23 -0700

    Added Public Protection Level to prettyPrintedJsonString

--- Sources/SwiftPlus/Extensions/Data+prettyPrintedJSONString().swift
+++ Sources/SwiftPlus/Extensions/Data+prettyPrintedJSONString().swift
@@ -11,7 +11,7 @@ extension Data {
     /// Converts a JSON Data object into a readable String. Useful for determining the format of data provided by an API, for example. If this method fails
     /// to convert a JSON Data object into a String, it will print an error message and return an empty String.
     /// - Returns: A readable, String representation of a given JSON Data object. If conversion fails, this value is an empty String.
-    func prettyPrintedJSONString() -> String {
+    public func prettyPrintedJSONString() -> String {
         guard let jsonObject = try? JSONSerialization.jsonObject(with: self, options: []),
               let jsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: [.prettyPrinted]),
               let prettyJSONString = String(data: jsonData, encoding: .utf8) else {
